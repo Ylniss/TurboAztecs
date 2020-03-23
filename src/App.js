ebb49ed4c867383000889b547b97062eafc85537
Yolan <zupqa0@gmail.com>
2020-03-23 21:34:04 +0100

    create component Panel

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import Panel from './components/menu/Panel'
 
 function App() {
   return (
     <div>
-      
+      <Panel width='500px' height='300px'>
+        <h1>First window</h1>
+      </Panel>
     </div>
   );
 }
---
+++ src/components/menu/Panel.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import PropTypes from "prop-types";
+
+export default function Panel(props) {
+  
+  const panelStyle = {
+    background: '#383838',
+    border: 'none',
+    padding: '25px',
+    borderRadius: '10px',
+    width: props.width,
+    height: props.height,
+
+    //center horizontaly and vertically
+    margin: 0,
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)'
+  }
+  
+  return (
+    <div style={panelStyle}>
+      {props.children}
+    </div>
+  )
+}
+
+Panel.propTypes = {
+  width: PropTypes.string.isRequired,
+  height: PropTypes.string.isRequired
+}
