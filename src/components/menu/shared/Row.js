1a957f00352d5533eb80669c586c9b2caa65d050
Yolan <zupqa0@gmail.com>
2020-03-26 22:57:57 +0100

    refactor panel, row; set players from context

--- src/components/menu/shared/Row.js
+++ src/components/menu/shared/Row.js
@@ -1,15 +1,15 @@
 import React from 'react'
 
-export default function Row(props) {
+export default function Row({ children, size, direction }) {
   const rowStyle = {
     display: 'flex',
-    flexDirection: props.direction === 'row' ? 'row' : 'column',
-    flex: props.size
+    flexDirection: direction === 'row' ? 'row' : 'column',
+    flex: size
   }
 
   return (
     <div style={rowStyle}>
-      {props.children}
+      {children}
     </div>
   )
 }
