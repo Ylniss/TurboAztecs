154fe1abaf8df34553afda137796de813d3d2693
Yolan <zupqa0@gmail.com>
2020-03-27 15:23:55 +0100

    comment

--- src/components/game/Table.js
+++ src/components/game/Table.js
@@ -4,7 +4,7 @@ import tableImage from '../../assets/img/table/table.jpg';
 import boardImage from '../../assets/img/table/board.jpg';
 
 export default function Table() {
-
+  //todo: get players from useContext and setup board accordingly
   return (
     <>
       <img  className='table' src={tableImage} alt='table'/>
